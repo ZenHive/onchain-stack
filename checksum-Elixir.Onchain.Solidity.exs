68b260bb7a1d01bbf88d597d4042ff8e5161dbf1
E.FU <e-fu@users.noreply.github.com>
2026-08-23 08:48:36 +0800

    release: commit v0.6.0 precompiled NIF checksums

---
+++ checksum-Elixir.Onchain.Solidity.exs
@@ -0,0 +1,7 @@
+%{
+  "libonchain_solidity-v0.6.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:8f1e7df8f9c5c4a63767897fb26c818dcf1ae3942327189473cb692e61f6c19a",
+  "libonchain_solidity-v0.6.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ab3de63572e30b6a3cfbcca56386c53c8db3f5db8f27706eba71d9ce12ac86bc",
+  "libonchain_solidity-v0.6.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:914bcbaa19ddad2fa8e5c46c735ce0fc6a084d909686b5bc2f137586a873961c",
+  "libonchain_solidity-v0.6.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:15a7b33531212658fb3c15c43ac1a72c87d05ae94dc965f1dca2613bfa2c6841",
+  "libonchain_solidity-v0.6.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:02a6b2e1e8374939814070b9310ebf6057598f686f33eef0df512ae3ce6dbb5e",
+}
